bece718043aff3987bc15a82235ea469b43a17c1
musfiquerprottoy <musfiquer36@gmail.com>
2025-02-28 23:51:13 +0600

    Task 02

--- Assignment-1/Task-02.py
+++ Assignment-1/Task-02.py
@@ -1,22 +1,21 @@
-T = int(input())
+T = int(input())  # Number of test cases
+
 for i in range(T): 
-    inp = input()
-    line = inp[10:]
-    parts = line.split( )
-    num1 = parts[0]
-    opt = parts[1]
-    num2 = parts[2]
+    inp = input().split()  # Split the input into parts
+    num1 = float(inp[1])  # First number
+    opt = inp[2]  # Operator (+, -, *, /)
+    num2 = float(inp[3])  # Second number
     rslt = 0
 
+    # Perform the calculation
     if opt == '+': 
-        rslt = int(num1) + int(num2)
-        print(f'{rslt:.6f}')
+        rslt = num1 + num2
     elif opt == '-': 
-        rslt = int(num1) - int(num2)
-        print(f'{rslt:.6f}')
+        rslt = num1 - num2
     elif opt == '*': 
-        rslt = int(num1) * int(num2)
-        print(f'{rslt:.6f}')
+        rslt = num1 * num2
     elif opt == '/':
-        rslt = int(num1) / int(num2)
-        print(f'{rslt:.6f}')
+        rslt = num1 / num2  # Division automatically handles float values
+
+    # Print the result with 6 decimal places
+    print(f'{rslt:.6f}')

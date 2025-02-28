66e71cab296675fc20879f1acfd3df77586366cc
musfiquerprottoy <musfiquer36@gmail.com>
2025-02-28 23:43:27 +0600

    Task 01

--- Assignment-1/Task-01.py
+++ Assignment-1/Task-01.py
@@ -1,1 +1,10 @@
-
+T = int(input())
+for i in range(T): 
+    N = int(input())
+    if -10**5 <= N <= 10**5: 
+        if N%2 == 0: 
+            print(f'{N} is an Even number.')
+        else: 
+            print(f'{N} is an Odd number.')
+    else: 
+        break 

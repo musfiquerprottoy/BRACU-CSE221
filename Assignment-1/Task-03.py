dbca03d99bd17dd1a46aa11004a3db91e9315816
musfiquerprottoy <musfiquer36@gmail.com>
2025-03-01 02:19:58 +0600

    Task 03

--- Assignment-1/Task-03.py
+++ Assignment-1/Task-03.py
@@ -1,13 +1,13 @@
 import numpy as np
+
 N = int(input())
 K = int(input())
+
 arr = np.zeros(N, dtype=int)
+
 for i in range(N): 
     arr[i] = int(input())
-arr2 = np.zeros(N, dtype=int)
-num = 0
-for i in range(N-1, -1, -1):
-    arr2[num] = arr[i]
-    num += 1  
+
+arr2 = arr[::-1]
 for j in range(N-K, N, 1): 
     print(arr2[j], end=' ')

3c0b11785d59e65726ae15fe92bcd9b1f3b343b8
musfiquerprottoy <musfiquer36@gmail.com>
2025-03-01 00:33:44 +0600

    Task 04

--- Assignment-1/Task-04.py
+++ Assignment-1/Task-04.py
@@ -1,5 +1,5 @@
-T = int(input('Please enter the number of test cases: '))
-for i in range(T): 
-    Num = int(input())
-    sum = ((Num*(Num+1))//2)
+T = int(input())
+for _ in range(T): 
+    num = int(input())
+    sum = ((num*(num+1))//2)
     print(sum)

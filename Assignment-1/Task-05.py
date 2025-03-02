9a07f647bd1b94bc67c3473458dab70f9dc7de5b
musfiquerprottoy <musfiquer36@gmail.com>
2025-03-02 11:23:29 +0600

    Task 05

--- Assignment-1/Task-05.py
+++ Assignment-1/Task-05.py
@@ -1,20 +1,10 @@
-def bubbleSort(arr, n):                                                 
+def bubbleSort(arr):
+    n = len(arr)
     for i in range(n - 1):
-        swapped = False  # Track if any swaps occur in this pass
-        
-        for j in range(n - i - 1): 
-            if arr[j] > arr[j + 1]:  
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # Swap
-                swapped = True  # Swap happened, so continue sorting
-        
-        if not swapped:  # If no swaps occurred, array is already sorted
-            break  
-
-# Taking input
-N = int(input())  # Read array size
-arr = list(map(int, input().split()))  # Read array elements
-
-bubbleSort(arr, N)  # Sort the array
-
-# Print the sorted array
-print(*arr)
+        swapped = False 
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True 
+        if not swapped: 
+            break

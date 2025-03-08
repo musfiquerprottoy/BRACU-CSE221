9b776ec707a0d9f1d83fb3b6d0072faa6e6ce07f
musfiquerprottoy <musfiquer36@gmail.com>
2025-03-08 22:51:25 +0600

    Task 02

--- Assignment-2/Task 02.py
+++ Assignment-2/Task 02.py
@@ -1,29 +1,28 @@
 def mergelist():
-    N = int(input().strip())
-    Alice = list(map(int, input().strip().split()))
+    N = int(input().strip())  # Size of Alice's list
+    Alice = list(map(int, input().strip().split()))  # Alice's list
 
     M = int(input().strip())
-    Bob = list(map(int, input().strip().split()))
+    Bob = list(map(int, input().strip().split()))  # Bob's list
 
     i, j = 0, 0
     merged_list = []
 
-    while i<N and j<M: 
-        if Alice[i]<= Bob[j]:
+    while i < N and j < M:  
+        if Alice[i] <= Bob[j]:  
             merged_list.append(Alice[i])
             i += 1
         else: 
             merged_list.append(Bob[j])
             j += 1
 
-    while i<N: 
+    while i < N:
         merged_list.append(Alice[i])
         i += 1
-
-    while j<M: 
+    while j < M:
         merged_list.append(Bob[j])
         j += 1
 
     print(" ".join(map(str, merged_list)))
 
-mergelist() 
+mergelist()

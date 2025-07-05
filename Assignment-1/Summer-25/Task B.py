d11aed64689a8605b0d69f8bda79dc8fc7b8d82b
Musfiquer Prottoy <142139761+musfiquerprottoy@users.noreply.github.com>
2025-07-05 23:12:50 +0600

    Create Task B.py

---
+++ Assignment-1/Summer-25/Task B.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    text = input().strip()
+    final = text.replace('calculate', '').strip()
+    result = eval(final)
+    print(f'{result:.6f}')

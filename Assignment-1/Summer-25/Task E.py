9911150b450760571d07db8e858707d2c46ae4dc
Musfiquer Prottoy <142139761+musfiquerprottoy@users.noreply.github.com>
2025-07-05 23:38:14 +0600

    Update Task E.py

--- Assignment-1/Summer-25/Task E.py
+++ Assignment-1/Summer-25/Task E.py
@@ -1,12 +1,46 @@
-n = int(input())
-arr = list(map(int, input().split()))
+def main():
+    import sys
+    data = sys.stdin.read().split()
+    n = int(data[0])
+    a = list(map(int, data[1:1+n]))
+    
+    if n == 1:
+        print("YES")
+    elif n == 2:
+        if a[0] <= a[1]:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        even = []
+        odd = []
+        for i in range(n):
+            if i % 2 == 0:
+                even.append(a[i])
+            else:
+                odd.append(a[i])
+                
+        even.sort()
+        odd.sort()
+        
+        b = []
+        index_even = 0
+        index_odd = 0
+        for i in range(n):
+            if i % 2 == 0:
+                b.append(even[index_even])
+                index_even += 1
+            else:
+                b.append(odd[index_odd])
+                index_odd += 1
+                
+        is_sorted = True
+        for i in range(1, n):
+            if b[i] < b[i-1]:
+                is_sorted = False
+                break
+                
+        print("YES" if is_sorted else "NO")
 
-unsorted_term = 0
-for i in range(n):
-    for j in range(i+1, n):
-        if arr[i]>arr[j]:
-            unsorted_term += 1
-if unsorted_term%2==0:
-    print("YES")
-else:
-    print("NO")
+if __name__ == "__main__":
+    main()

1487b4ecefeb9e00cf98c95386e9252238ec5570
Musfiquer Prottoy <142139761+musfiquerprottoy@users.noreply.github.com>
2025-07-11 21:33:11 +0600

    Update Task B.py

--- Summer25/Lab 02/Task B.py
+++ Summer25/Lab 02/Task B.py
@@ -1,1 +1,22 @@
+N, M, K = map(int,input().split())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int,input().split()))
 
+starting = 1
+ending = M
+best_starting = best_ending = 1
+min_diff = float('inf')
+
+while starting<=N and ending>=0:
+    curr_sum = arr1[starting-1] + arr2[ending-1]
+    curr_diff = abs(curr_sum - K)
+    if curr_diff < min_diff:
+        min_diff = curr_diff
+        best_starting = starting
+        best_ending = ending
+
+    if curr_sum < K:
+        starting += 1
+    else:
+        ending -= 1
+print(best_starting, best_ending)

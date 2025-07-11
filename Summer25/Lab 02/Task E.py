dddae726a234cfc28c5f59612f284d2f7011dc6a
Musfiquer Prottoy <142139761+musfiquerprottoy@users.noreply.github.com>
2025-07-11 21:35:20 +0600

    Update Task E.py

--- Summer25/Lab 02/Task E.py
+++ Summer25/Lab 02/Task E.py
@@ -1,1 +1,20 @@
+def longest_subarray(N, K, a):
+    l = 0
+    current_sum = 0
+    max_len = 0
 
+    for right in range(N):
+        current_sum = current_sum + a[right]
+        while current_sum > K:
+            current_sum = current_sum - a[l]
+            l += 1
+
+        max_len = max(max_len, right - l + 1)
+
+    return max_len
+
+
+N, K = map(int, input().split())
+a = list(map(int, input().split()))
+
+print(longest_subarray(N, K, a))

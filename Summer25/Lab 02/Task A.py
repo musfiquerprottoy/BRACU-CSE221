fdc1d1bc21def58c14c9b04b1b8557d7f76573f8
Musfiquer Prottoy <142139761+musfiquerprottoy@users.noreply.github.com>
2025-07-11 21:31:44 +0600

    Update Task A.py

--- Summer25/Lab 02/Task A.py
+++ Summer25/Lab 02/Task A.py
@@ -1,1 +1,19 @@
+N, target_sum = map(int,input().split())
+arr = list(map(int, input().split()))
+found = False
 
+starting = 1
+ending = N
+
+while starting < ending:
+    current_sum = arr[starting-1] + arr[ending-1]
+    if current_sum == target_sum:
+        print(starting, ending)
+        found = True
+        break
+    elif current_sum < target_sum:
+        starting += 1
+    else:
+        ending -= 1
+if not found:
+    print(-1)
